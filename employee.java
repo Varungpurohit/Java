0578cf24bfdf4579ae43308b9883be8564e8c097
Varungpurohit <32390250+Varungpurohit@users.noreply.github.com>
2022-05-20 12:50:13 +0530

    Create employee.java

---
+++ employee.java
@@ -0,0 +1,53 @@
+package com.varun;
+
+public class Employee {
+	
+	int empid;
+    String name;
+    int salary;
+    
+    public Employee(int empid, String name, int salary) {
+		super();
+		this.empid = empid;
+		this.name = name;
+		this.salary = salary;
+	}
+
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getEmpid() {
+		return empid;
+	}
+
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empid=" + empid + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+
+}
