4768f616b3bff8782beb967a7ca428617e1cebd2
Varungpurohit <32390250+Varungpurohit@users.noreply.github.com>
2022-05-20 12:53:58 +0530

    Create client.java

---
+++ client.java
@@ -0,0 +1,16 @@
+package com.varun;
+
+public class Client {
+
+	public static void main(String[] args) {
+		     
+				Employee e= new Employee();
+				e.setEmpid(10);
+				e.setName("Varun");
+				e.setSalary(1000);
+     System.out.println(e);
+		
+
+	}
+
+}
